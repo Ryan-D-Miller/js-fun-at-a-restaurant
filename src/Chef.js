14a451a5fc3f8dbcbe0572697ab4704ffe57fc94
Ryan Miller <ryandmiller3521@gmail.com>
2021-02-07 17:28:42 -0700

    Added Chef object along with its test

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,26 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
+  }
+
+  greetCustomer(customerName, isMorning){
+    if(isMorning === true){
+      return `Good morning, ${customerName}!`;
+    }
+    else{
+      return `Hello, ${customerName}!`;
+    }
+  }
+
+  checkForFood(foodItem){
+    for(var i = 0; i < this.restaurant.menus[foodItem.type].length; i++){
+      console.log("Im here!");
+      if(this.restaurant.menus[foodItem.type][i].name === foodItem.name){
+        return `Yes, we're serving ${foodItem.name} today!`;
+      }
+    }
+    return `Sorry, we aren't serving ${foodItem.name} today.`;
   }
 }
 
